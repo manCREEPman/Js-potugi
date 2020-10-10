ebf170801a0c0693b57e48189eaa931e32d93d28
manCREEPman <barbekyan@yandex.ru>
2020-10-10 12:23:07 +0300

    понерфлен отскок

--- valun/script.js
+++ valun/script.js
@@ -5,6 +5,9 @@ const ball = {
   speed: 10,
   currentSpeed: 10,
   fallState: false,
+  fallStartHeight: 0,
+  ballEndHeight: 0,
+  direction: 1,
   fallStartTime: null,
   posX: 0,
   posY: 0,
@@ -20,6 +23,13 @@ function isBallNearGround() {
   else return false;
 }
 
+function isBallNearBounceHeight(){
+  heightArea = 10;
+  if (ball.posY <= ball.fallStartHeight && 
+    ball.direction < 0) return true;
+  return false;
+}
+
 function changeBallCoords(x, y) {
   ball.posX = x;
   ball.posY = y;
@@ -30,27 +40,55 @@ function placeBall() {
   ball.object.style.top = ball.posY + "px";
 }
 
-function increaseCurrentSpeed(direction) {
+function changeDirection(){
+  ball.direction *= -1;
+  ball.fallStartHeight = field.bottomBorder - (field.bottomBorder - ball.fallStartHeight) * 2 / 3;
+}
+
+
+function increaseCurrentSpeed() {
   if (!ball.fallState) {
+    ball.fallStartHeight = ball.posY;
     ball.fallState = true;
     ball.fallStartTime = new Date();
   }
   tempTime = new Date();
   deltaTime = new Date(+tempTime - ball.fallStartTime).getMilliseconds();
-  ball.currentSpeed += (direction * 10 * deltaTime) / 1000;
+  ball.currentSpeed += (ball.direction * 10 * deltaTime) / 1000;
+}
+
+function deltaClause(){
+  return (field.bottomBorder - ball.fallStartHeight <= parseInt(ball.ballCompStyle.width));
 }
 
 function fallingGravitation() {
-  if (!isBallNearGround()) {
-    increaseCurrentSpeed(1);
-    console.log(ball.currentSpeed);
-    ball.posY += ball.currentSpeed;
-    if (ball.posY > field.bottomBorder) ball.posY = field.bottomBorder;
-    placeBall();
-  } else {
+  if(isBallNearGround() && deltaClause()){
     ball.fallState = false;
     ball.currentSpeed = ball.speed;
+    ball.direction = 1;
   }
+  else{
+    if(ball.fallState && isBallNearGround()){
+      changeDirection();
+    }
+    else
+    if(ball.fallState && isBallNearBounceHeight()){
+      changeDirection();
+      ball.fallStartTime = new Date();
+      ball.currentSpeed = 0;
+    }
+    increaseCurrentSpeed();
+    ball.posY += ball.direction * ball.currentSpeed;
+    if (ball.posY >= field.bottomBorder) ball.posY = field.bottomBorder;
+    placeBall();
+  }
+  console.log("===========================")
+  console.log("speed", ball.currentSpeed);
+  console.log("direction", ball.direction);
+  console.log("ball fall state", ball.fallState);
+  console.log("bounce height", ball.fallStartHeight);
+  console.log("current height", ball.posY);
+  console.log("delta", field.bottomBorder - ball.fallStartHeight);
 }
 
 function initFieldBorders() {
@@ -71,12 +109,16 @@ function initBallPosition() {
   ball.posY = parseInt(ball.ballCompStyle.top);
 }
 
+
+ball.onmousedown() = function () {
+
+}
+
 initBallPosition();
 initFieldBorders();
 eventTimer = setInterval(fallingGravitation, 100);
 
 /*
-    ускорение
-    отскок
+    перетаскивание
     внешние объекты
 */
